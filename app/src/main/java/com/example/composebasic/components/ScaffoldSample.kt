6a34ad8f2873142eb9b906399605dd1639a0734a
liaojie1314 <1517438366@qq.com>
2022-03-09 10:02:13 +0800

    ScaffoldSample

---
+++ app/src/main/java/com/example/composebasic/components/ScaffoldSample.kt
@@ -0,0 +1,235 @@
+package com.example.composebasic.components
+
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ScaffoldSample() {
+    val bottomData = listOf("Home", "List", "User")
+    var bottomNavigationCurrentIndex by remember {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Title") }, navigationIcon = {
+                Icon(imageVector = Icons.Default.NavigateBefore, contentDescription = null)
+            }, actions = {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            })
+        },
+        bottomBar = {
+            BottomNavigation {
+                bottomData.forEachIndexed { index, item ->
+                    BottomNavigationItem(
+                        selected = bottomNavigationCurrentIndex == index,
+                        onClick = { bottomNavigationCurrentIndex = index }, icon = {
+                            BadgeBox(badgeContent = {
+                                Text("1")
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.AccountBox,
+                                    contentDescription = null
+                                )
+                            }
+                        }, label = { Text(item) })
+                }
+            }
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.SupervisedUserCircle,
+                        contentDescription = null
+                    )
+                },
+                text = { Text("Button") },
+                onClick = { /*TODO*/ })
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true
+    ) {
+        Text("Body Content")
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ScaffoldSample1() {
+    val scope = rememberCoroutineScope()
+    val selection = remember { mutableStateOf(1) }
+    val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+    LaunchedEffect(scaffoldState) {
+        scaffoldState.reveal()
+    }
+    BackdropScaffold(
+        scaffoldState = scaffoldState,
+        appBar = {
+            TopAppBar(
+                title = { Text("Backdrop scaffold") },
+                navigationIcon = {
+                    if (scaffoldState.isConcealed) {
+                        IconButton(onClick = { scope.launch { scaffoldState.reveal() } }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Localized description")
+                        }
+                    } else {
+                        IconButton(onClick = { scope.launch { scaffoldState.conceal() } }) {
+                            Icon(Icons.Default.Close, contentDescription = "Localized description")
+                        }
+                    }
+                },
+                actions = {
+                    var clickCount by remember { mutableStateOf(0) }
+                    IconButton(
+                        onClick = {
+                            // show snackbar as a suspend function
+                            scope.launch {
+                                scaffoldState.snackbarHostState
+                                    .showSnackbar("Snackbar #${++clickCount}")
+                            }
+                        }
+                    ) {
+                        Icon(Icons.Default.Favorite, contentDescription = "Localized description")
+                    }
+                },
+                elevation = 0.dp,
+                backgroundColor = Color.Transparent
+            )
+        },
+        backLayerContent = {
+            LazyColumn {
+                items(if (selection.value >= 3) 3 else 5) {
+                    ListItem(
+                        Modifier.clickable {
+                            selection.value = it
+                            scope.launch { scaffoldState.conceal() }
+                        },
+                        text = { Text("Select $it") }
+                    )
+                }
+            }
+        },
+        frontLayerContent = {
+            Text("Selection: ${selection.value}")
+            LazyColumn {
+                items(50) {
+                    ListItem(
+                        text = { Text("Item $it") },
+                        icon = {
+                            Icon(
+                                Icons.Default.Favorite,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ScaffoldSample2() {
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    BottomSheetScaffold(
+        sheetContent = {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(128.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Swipe up to expand sheet")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(64.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sheet content")
+                Spacer(Modifier.height(20.dp))
+                Button(
+                    onClick = {
+                        scope.launch { scaffoldState.bottomSheetState.collapse() }
+                    }
+                ) {
+                    Text("Click to collapse sheet")
+                }
+            }
+        },
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = { Text("Bottom sheet scaffold") },
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { scaffoldState.drawerState.open() } }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Localized description")
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            var clickCount by remember { mutableStateOf(0) }
+            FloatingActionButton(
+                onClick = {
+                    // show snackbar as a suspend function
+                    scope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar("Snackbar #${++clickCount}")
+                    }
+                }
+            ) {
+                Icon(Icons.Default.Favorite, contentDescription = "Localized description")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        sheetPeekHeight = 128.dp,
+        drawerContent = {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Drawer content")
+                Spacer(Modifier.height(20.dp))
+                Button(onClick = { scope.launch { scaffoldState.drawerState.close() } }) {
+                    Text("Click to close drawer")
+                }
+            }
+        }
+    ) { innerPadding ->
+        LazyColumn(contentPadding = innerPadding) {
+            items(100) {
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScaffoldSamplePreview() {
+    ScaffoldSample2()
+}
+
